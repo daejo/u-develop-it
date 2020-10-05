4495d24466a3477389230803e046f9f9e47135a6
daejo <daejo@icloud.com>
2020-10-05 02:27:16 -0700

    adds a new candidate thru server interaction

--- server.js
+++ server.js
@@ -23,6 +23,38 @@ const db = new sqlite3.Database('./db/election.db', err => {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// // GET a single candidate
+// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if(err) {
+//       console.log(err);
+//     }
+//     console.log(row);
+//   });
+
+// // Delete a candidate
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result, this, this.changes);
+//   });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+db.run(sql, params, function(err, result) {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result, this.lastID);
+});
+
 // Default response for any other requests(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
